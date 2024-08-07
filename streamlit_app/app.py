94556ffb83e93e7885da72b51d5ca53299188687
Izabela Ramos Ferreira <izabela.head@gmail.com>
2024-08-06 21:58:54 -0300

    end

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,82 +1,67 @@
-import streamlit as st
+import altair as alt
 import numpy as np
-from PIL import Image
-import tensorflow as tf
+import pandas as pd
+import streamlit as st
 import io
-import os
+from PIL import Image
+import subprocess
+import sys
+import cv2
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 import dlib
+import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-model_path = 'streamlit_app/xmodel.h5'
-xmodel = tf.keras.models.load_model(model_path)
+model_path = '/Users/bela/Documents/GitHub/cognitive-environments/streamlit_app/model.h5'
+liveness_model = load_model(model_path)
 
-
+# Carregar o detector de faces dlib
 detector = dlib.get_frontal_face_detector()
 
-
+# Função para detectar e extrair o rosto
 def detect_and_extract_face(image):
     img_array = np.array(image)
     faces = detector(img_array, 1)
     for i, d in enumerate(faces):
         x, y, w, h = d.left(), d.top(), d.width(), d.height()
         roi_color = img_array[y : y + h, x : x + w]
-        face_img = Image.fromarray(roi_color).resize((224, 224))
+        face_img = Image.fromarray(roi_color).resize((128, 128))
         return face_img, (x, y, w, h)
     return None, None
 
-
+# Função para classificar a vivacidade
 def classify_liveness(face_img):
     face_array = np.array(face_img) / 255.0
-    face_tensor = np.expand_dims(face_array, axis=0)
-    prediction = xmodel.predict(face_tensor)
+    face_tensor = np.expand_dims(face_array, axis=0)  # Keep the shape as (1, 128, 128, 3)
+    print("Shape of face tensor before passing to model:", face_tensor.shape)  # Debugging
+    prediction = liveness_model.predict(face_tensor)
     return prediction[0][0]
 
-
-st.title('Cognitive Environments - Detecção de Vivacidade')
+# Streamlit interface
+st.title("| Cognitive Environments | FIAP | 2024 |")
 st.header("| Izabela Ramos Ferreira      | RM 352447")
 st.header("| Kaique Vinicius Lima Soares | RM 351437")
 st.header("| Walder Octacilio Garbellott | RM 352469")
-def capture_image():
-    picture = st.camera_input("Tire uma foto")
-    return picture
 
-option = st.radio("Escolha uma opção:", ('Carregar Imagem', 'Capturar pela Webcam'))
 
-uploaded_file = None
-camera = None
+uploaded_file = st.file_uploader("Tente uma outra imagem", type=["png", "jpg"])
+camera = st.camera_input(
+    "Tire sua foto",
+    help="Lembre-se de permitir ao seu navegador o acesso a sua câmera.",
+)
 
-if option == 'Carregar Imagem':
-    uploaded_file = st.file_uploader("Escolha uma imagem...", type=["jpg", "jpeg"])
-else:
-    camera = capture_image()
+if camera or uploaded_file:
+    if uploaded_file is not None:
+        img_stream = io.BytesIO(uploaded_file.getvalue())
+        imagem = Image.open(img_stream).convert("RGB")
+    elif camera is not None:
+        bytes_data = camera.getvalue()
+        imagem = Image.open(io.BytesIO(bytes_data)).convert("RGB")
 
-def process_image(image_file):
-    try:
-        if image_file is not None:
-            img_stream = io.BytesIO(image_file.getvalue())
-            image = Image.open(img_stream).convert("RGB")
-            return image
-    except Exception as e:
-        st.error(f"Erro ao processar a imagem: {e}")
-    return None
-
-image = None
-
-if uploaded_file:
-    image = process_image(uploaded_file)
-elif camera:
-    image = process_image(camera)
-
-if image:
     with st.spinner("Classificando imagem..."):
-        image = image.resize((128, 128))
-        image_array = np.array(image) / 255.0
-        image_array = np.expand_dims(image_array, axis=0)
-
-        st.write("Tamanho da imagem:", image_array.shape)
-
-        face_img, bbox = detect_and_extract_face(image)
+        face_img, bbox = detect_and_extract_face(imagem)
         if face_img is not None:
             prediction = classify_liveness(face_img)
             if prediction >= 0.5:
@@ -86,8 +71,10 @@ if image:
                 result = "Rosto Forjado"
                 color = (0, 0, 255)
 
+            # Desenhar a bounding box na imagem original
             x, y, w, h = bbox
-            imagem_np = np.array(image)
+            imagem_np = np.array(imagem)
+            cv2.rectangle(imagem_np, (x, y), (x + w, y + h), color, 2)
 
             st.image(imagem_np, channels="RGB")
             st.success(
@@ -97,5 +84,3 @@ if image:
             st.error(
                 "Nenhum rosto detectado. Tente novamente com uma imagem diferente."
             )
-else:
-    st.info("Por favor, carregue uma imagem ou capture uma foto usando a webcam.")
